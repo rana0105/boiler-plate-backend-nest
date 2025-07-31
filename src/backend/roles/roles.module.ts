81e01163ca762a0a075d40885ff04bf701817751
Morshed Khan Rana <morshed.rnaa@shohoz.com>
2025-07-31 23:56:38 +0600

    some permission issue fix with prefix add api

--- src/backend/roles/roles.module.ts
+++ src/backend/roles/roles.module.ts
@@ -4,11 +4,15 @@ import { RolesController } from './roles.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './role.entity';
 import { Permission } from '../permissions/permission.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Role, Permission]),
+    UsersModule, 
+  ],
   providers: [RolesService],
   controllers: [RolesController],
   exports: [RolesService],
-  imports: [TypeOrmModule.forFeature([Role, Permission])],
 })
 export class RolesModule {}
